4c8422b362962281b9a7d076bc1c01496dba0e86
Jim Chung <jchung225@gmail.com>
2020-01-06 18:44:10 -0500

    herok

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type: 'application/vin.api+json'}));
 
 
-require('./app/routing/apiRoutes')(app);
-require('./app/routing/htmlRoutes')(app);
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function() {
   console.log('App is listening on PORT: ' + 3000);
