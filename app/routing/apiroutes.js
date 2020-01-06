826d03bc0f780068cede9f564f3cb9416007893d
Jim Chung <jchung225@gmail.com>
2020-01-05 23:15:31 -0500

    tweaks

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -2,15 +2,12 @@
 var path = require('path');
 var friends = require('../data/friends.js');
 
-// Export API routes
 module.exports = function(app) {
 
-	// Total list of friend entries
 	app.get('/api/friends', function(req, res) {
 		res.json(friends);
 	});
 
-	// Add new friend entry
 	app.post('/api/friends', function(req, res) {
 		var userInput = req.body;
 		console.log('userInput = ' + JSON.stringify(userInput));
