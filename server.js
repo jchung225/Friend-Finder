39ffc058c0293fac9cebeb7080b6c0a317718ebb
Jim Chung <jchung225@gmail.com>
2020-01-06 19:04:54 -0500

    removed body parser

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.text());
-app.use(bodyParser.json({type: 'application/vin.api+json'}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 require('./app/routing/apiRoutes.js')(app);
